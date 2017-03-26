5ce9d75cef6736de4f83ff740e762444818fb8ab
Hicham MERBOUH <umerbhi@vm1243515.ile.mtl.qc.ca>
2017-03-26 19:51:37 -0400

    First commit
    First commit

---
+++ www/js/controllers.js
@@ -0,0 +1,22 @@
+angular.module('app.controllers', ['ja.qr'])
+     
+.controller('matchScoutingCtrl', ['$scope', '$stateParams', 
+function ($scope, $stateParams, $ionicPopup) {
+  $scope.size = 250;
+  $scope.correctionLevel = '';
+  $scope.typeNumber = 0;
+  $scope.inputMode = '';
+  $scope.image = true;
+
+}])
+   
+.controller('pitScoutingCtrl', ['$scope', '$stateParams', 
+function ($scope, $stateParams) {
+  $scope.size = 250;
+  $scope.correctionLevel = '';
+  $scope.typeNumber = 0;
+  $scope.inputMode = '';
+  $scope.image = true;
+
+}])
+ 
