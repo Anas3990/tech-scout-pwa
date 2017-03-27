29cd1352a8ecbdc9cef605b4b64681c5b174f0e7
Hicham MERBOUH <umerbhi@vm1243515.ile.mtl.qc.ca>
2017-03-26 20:21:36 -0400

    Added "Générer le code QR !" button

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,16 +1,15 @@
 angular.module('app.controllers', ['ja.qr'])
      
-.controller('matchScoutingCtrl', ['$scope', '$stateParams', 
+.controller('matchScoutingCtrl', ['$scope', '$stateParams',
 function ($scope, $stateParams, $ionicPopup) {
   $scope.size = 250;
   $scope.correctionLevel = '';
   $scope.typeNumber = 0;
   $scope.inputMode = '';
   $scope.image = true;
-
 }])
    
-.controller('pitScoutingCtrl', ['$scope', '$stateParams', 
+.controller('pitScoutingCtrl', ['$scope', '$stateParams',  
 function ($scope, $stateParams) {
   $scope.size = 250;
   $scope.correctionLevel = '';
